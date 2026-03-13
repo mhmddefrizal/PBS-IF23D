d9f797d5e378ac58109b54e75004eea4817a4092
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-03-13 16:43:45 +0700

    Add CreateKategoriDto and implement kategori creation logic with conflict handling

--- api/kategori/src/kategori/kategori.service.ts
+++ api/kategori/src/kategori/kategori.service.ts
@@ -1,4 +1,10 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 import { UpdateKategoriDto } from './dto/update-kategori.dto';
 import { PrismaService } from '../prisma.service';
@@ -9,14 +15,81 @@ export class KategoriService {
   // buat constructor untuk inject prisma service
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createKategoriDto: CreateKategoriDto) {
-    return 'This action adds a new kategori';
+  async create(createKategoriDto: CreateKategoriDto) {
+    // return 'This action adds a new kategori';
+
+    // buat variabel untuk filter nama
+    const nama_filter = createKategoriDto.nama
+      .replace(/\s/g, '')
+      .toLowerCase()
+      .trim();
+
+    // cek apakah nama kategori sudah ada
+    const exist = await this.prisma.kategori.findFirst({
+      where: {
+        nama: nama_filter,
+      },
+    });
+
+    // jika nama kategori sudah ada
+    if (exist) {
+      throw new ConflictException({
+        success: false,
+        message: 'Data kategori Gagal disimpan!, nama kategori sudah ada.',
+        metadata: {
+          status: HttpStatus.CONFLICT,
+        },
+      });
+    }
+
+    // jika nama kategori belum ada
+
+    // simpan data kategori
+    await this.prisma.kategori.create({
+      data: {
+        nama: createKategoriDto.nama,
+      },
+    });
+
+    return {
+      success: true,
+      message: 'Data kategori berhasil disimpan',
+      metadata: {
+        status: HttpStatus.CREATED,
+      },
+    };
   }
 
   // buat fungsi untuk tampil data kategori
   async findAll() {
     // tampilkan data kategori
     const data = await this.prisma.kategori.findMany();
+
+    // jika data kategori tidak ditemukan
+    if (data.length === 0) {
+      // throw new HttpException(
+      //   {
+      //     success: false,
+      //     message: 'Data kategori tidak ditemukan',
+      //     metadata: {
+      //       status: HttpStatus.NOT_FOUND,
+      //       total_data: data.length,
+      //     },
+      //   },
+      //   HttpStatus.NOT_FOUND,
+      // );
+
+      throw new NotFoundException({
+        success: false,
+        message: 'Data kategori tidak ditemukan',
+        metadata: {
+          status: HttpStatus.NOT_FOUND,
+          total_data: data.length,
+        },
+      });
+    }
+
+    // jika data kategori ditemukan
     return {
       success: true,
       message: 'Data kategori',
